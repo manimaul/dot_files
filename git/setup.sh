c0fd287b1cfae7cc16816af08d155dd569282ed3
William Kamp <manimaul@gmail.com>
2020-04-26 17:03:37 -0700

    add git config

--- git/setup.sh
+++ git/setup.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ ${SOURCE} != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 
-simlink() {
-	echo "symlinking gitconfig"
-	gitconfig="$HOME/.gitconfig"
-	if [[ -e "$gitconfig" || -L "$gitconfig" ]]; then
-    if [ ! -L "$gitconfig" ];  then
+sim_link() {
+  echo "sym linking git config"
+  git_config="${HOME}/.gitconfig"
+  if [[ -e "${git_config}" || -L "$git_config" ]];  then
+    if [[ ! -L "$git_config" ]];  then
       echo "backing up ~/.gitconfig"
-      # not a symlink
-      cp $HOME/.gitconfig $/HOME.gitconfig.bak
+      cp ${HOME}/.gitconfig ${HOME}/.gitconfig.bak
     fi
     echo "deleting the ~/.gitconfig"
-    rm $HOME/.gitconfig
-	fi
-
-  if [[ -e $HOME/.gitignore_global ]]; then
-    rm $HOME/.gitignore_global
+    rm ${HOME}/.gitconfig
   fi
-	ln -s $DIR/gitconfig $HOME/.gitconfig
-  ln -s $DIR/gitignore_global $HOME/.gitignore_global
+
+  if [[ -e ${HOME}/.gitignore_global ]]; then
+    rm ${HOME}/.gitignore_global
+  fi
+  ln -s ${DIR}/gitconfig ${HOME}/.gitconfig
+  ln -s ${DIR}/gitignore_global ${HOME}/.gitignore_global
 }
 
 usage() {
@@ -35,7 +34,7 @@ usage() {
 
 case $1 in
 "link")
-	simlink
+	sim_link
 ;;
 *)
 	usage
